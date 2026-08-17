71e50ef6d73817d60981ffa57d00d5cf4d41d068
Kruban Nallainathan <nallainathan.kruban@gmail.com>
2026-08-17 21:49:30 +0530

    Add files via upload

---
+++ Tamil-to-English.py
@@ -0,0 +1,14 @@
+from googletrans import Translator
+
+# 1) Initialize
+translator = Translator()
+
+# 2) Translate Tamil → English
+Text = input("Enter the text in English")
+result = translator.translate(
+    Text,
+    src='en',
+    dest='ta'
+)
+
+print(result.text)
